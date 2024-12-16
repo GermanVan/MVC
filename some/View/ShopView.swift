05d668bc4b9ad171c5cf8dbfe16d5ffa0b662fe9
GermanVan <supergera95@gmail.com>
2024-12-16 15:08:40 -0800

    Fresh Commit

--- some/View/ShopView.swift
+++ some/View/ShopView.swift
@@ -7,15 +7,15 @@ class ShopView: UIView {
         tableView.estimatedRowHeight = 50
         tableView.rowHeight = 100
     }
-
+    
     func hideBorders (view: UIView) {
-        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut, animations: <#() -> Void#>, animations: ): {
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut, animations: {
             self.createViewHeighConstraint.constant = view.bounds.height * 0.6
             self.editButton.layer.cornerRadius = 20
             self.postView.layer?.shadowOpacity = 0
             self.postView.layer?.cornerRadius = 0
             view.layoutIfNeeded()
-        }, completion: nil
-                       }
-                       }
+        }, completion: nil)
+    }
+}
 
