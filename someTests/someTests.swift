b78e4b51d29f04578c3becfe249851f358bef5fd
GermanVan <supergera95@gmail.com>
2024-12-15 23:02:43 -0800

    Initial commit

---
+++ someTests/someTests.swift
@@ -0,0 +1,17 @@
+//
+//  someTests.swift
+//  someTests
+//
+//  Created by Герман Ван on 13.12.2024.
+//
+
+import Testing
+@testable import some
+
+struct someTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
